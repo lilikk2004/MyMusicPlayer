02203f3ec12ad6e58e2228abbac3367c2f900f89
oscar <344361912@qq.com>
2015-11-10 23:04:18 +0800

    Begin to make the remoteview;

--- app/src/main/java/study/oscar/player/manager/RemoteViewManager.java
+++ app/src/main/java/study/oscar/player/manager/RemoteViewManager.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.widget.RemoteViews;
 
 import study.oscar.player.R;
@@ -25,12 +26,14 @@ public class RemoteViewManager {
     }
 
     Context mContext;
+    Notification mNotification = null;
+    NotificationManager mNotifyManager = null;
 
     RemoteViewManager(Context context){
         mContext = context;
     }
 
-    public NotificationManager initView() {
+    public void initView() {
         RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(),
                 R.layout.statusbar);
         //设置按钮事件
@@ -50,20 +53,20 @@ public class RemoteViewManager {
         PendingIntent nextPi = PendingIntent.getService(mContext, 2, nextIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         remoteViews.setOnClickPendingIntent(R.id.statusBar_next, nextPi);//----设置对应的按钮ID监控
 
-/*        Intent closeIntent=new Intent(this,PlayerReceiver.class);
-        closeIntent.putExtra("action", "close");
-        closeIntent.setAction("com.lttclaw.playerReceiver");
-        PendingIntent closepi=PendingIntent.getBroadcast(this, 0, closeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        remoteViews.setOnClickPendingIntent(R.id.close, closepi);*/
-
         Notification.Builder builder = new Notification.Builder(mContext);
         builder.setContent(remoteViews).setSmallIcon(R.mipmap.ic_launcher)
                 .setOngoing(true)
                 .setTicker("music is playing");
-        Notification notification=builder.build();
-        //MyApplication.getInstance().notification=notification;
-        NotificationManager manager=(NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
-        manager.notify(1, notification);
-        return manager;
+        mNotification = builder.build();
+        mNotifyManager = (NotificationManager)mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotifyManager.notify(1, mNotification);
+    }
+
+    public void setSongCover(Bitmap bitmap){
+        if(mNotification == null || mNotifyManager == null){
+            return;
+        }
+        mNotification.contentView.setImageViewBitmap(R.id.remote_icon, bitmap);
+        mNotifyManager.notify(1, mNotification);
     }
 }
