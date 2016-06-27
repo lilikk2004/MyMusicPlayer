6c9964409ec8fed59707d6e92c358b6c649fd8d6
hyong15 <136150782@qq.com>
2016-06-27 17:14:11 +0800

    Add the background controler

--- app/src/main/java/study/oscar/player/ui/control/ImageBkView.java
+++ app/src/main/java/study/oscar/player/ui/control/ImageBkView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Handler;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,7 +20,7 @@ public class ImageBkView extends View {
     private Bitmap bitmapA = null;
     private Bitmap bitmapB = null;
     private int mode = IMG_NULL;
-    private int alphaA = 100;
+    private int alphaA = 0;
 
     final static public int IMG_NULL = 0;
     final static public int IMG_A = 1;
@@ -47,7 +48,55 @@ public class ImageBkView extends View {
 
         paint.setAlpha(alphaA);
         canvas.drawBitmap(bitmapA, 0, 0, paint);
+        paint.setAlpha(255 - alphaA);
     }
 
+    void setBitmap(Bitmap bitmap){
+        if(mode == IMG_NULL){
+            alphaA = 255;
+            mode = IMG_A;
+            bitmapA = bitmap;
+            postInvalidate();
+            return;
+        }
+        if(mode == IMG_A){
+            mode = IMG_B;
+            bitmapB = bitmap;
+            return;
+        }
+        if(mode == IMG_B){
+            mode = IMG_A;
+            bitmapB = bitmap;
+            return;
+        }
+    }
+
+
+    private final static int CHANGE_DELAY = 20;
+    private final static int PER_ALPHA = 15;
+
+    private Handler mHandler = new Handler();
+
+    private Runnable mRunnableChange = new Runnable() {
+        @Override
+        public void run() {
+            if (mode == IMG_B) {
+                alphaA -= PER_ALPHA;
+                if(alphaA <= 0){
+                    alphaA = 0;
+                }else {
+                    mHandler.postDelayed(mRunnableChange, CHANGE_DELAY);
+                }
+            }else if(mode == IMG_A){
+                alphaA += PER_ALPHA;
+                if(alphaA >= 255){
+                    alphaA = 255;
+                }else {
+                    mHandler.postDelayed(mRunnableChange, CHANGE_DELAY);
+                }
+            }
+            postInvalidate();
+        }
+    };
 
 }
